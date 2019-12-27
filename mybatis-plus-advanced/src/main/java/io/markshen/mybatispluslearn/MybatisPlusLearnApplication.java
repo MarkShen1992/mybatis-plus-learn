fb65e1eb72f98d314708c3ac45476e7e21979500
e <18009838166@163.com>
2019-12-27 20:37:59 +0800

    mybatis-plus-learn

---
+++ mybatis-plus-advanced/src/main/java/io/markshen/mybatispluslearn/MybatisPlusLearnApplication.java
@@ -0,0 +1,13 @@
+package io.markshen.mybatispluslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusLearnApplication.class, args);
+	}
+
+}
